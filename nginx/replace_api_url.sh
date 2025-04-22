76a71c57823790eae0b7aa85f05278b463e19e08
Omar <omar.bh1501@gmail.com>
2025-04-22 20:21:37 +0100

    Nginx Configuration

--- nginx/replace_api_url.sh
+++ nginx/replace_api_url.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+
 find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,PLACEHOLDER_REACT_APP_API_URL,'"$REACT_APP_API_URL"',g' {} \;
 find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,PLACEHOLDER_REACT_APP_MAP_KEY,'"$REACT_APP_MAP_KEY"',g' {} \;
 find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,PLACEHOLDER_REACT_APP_HR_URL,'"$REACT_APP_HR_URL"',g' {} \;
